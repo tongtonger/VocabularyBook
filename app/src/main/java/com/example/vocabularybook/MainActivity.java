5a2e84e10cc9304414ae22ffa2dbd6a9ba1cb72d
Tong <1342490381@qq.com>
2019-11-28 23:13:35 +0800

    add

--- app/src/main/java/com/example/vocabularybook/MainActivity.java
+++ app/src/main/java/com/example/vocabularybook/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                             Toast.makeText(MainActivity.this, "添加成功", Toast.LENGTH_LONG).show();
+
                     }
                 });
 
